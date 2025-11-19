0eafc9a041ece046fa5507ef5d9ca876dbecbeaf
Pedro Galvani <galvanipedro11@gmail.com>
2025-11-19 16:10:17 -0300

    wip

--- src/routes/asaas-anticipations/anticipation-page/AnticipationsTable.tsx
+++ src/routes/asaas-anticipations/anticipation-page/AnticipationsTable.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
